ae5cfb02e5eb22cbfd138a60a9a976b3e4c43e52
Hessam Imen <hessam.imen@gmail.com>
2022-09-25 16:01:46 +1000

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "welcome#home"
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/auth/github/callback' => 'sessions#create'
+
   # Add your routes here
 end
